e8e7edd5d091eb83b43db130b2d85f8ef6c64233
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2020-09-20 10:09:21 +0600

    px plot added

--- Poisson.py
+++ Poisson.py
@@ -26,7 +26,7 @@ def getExperimentalValues(cdfs):
     discreteValues = []
 
     seed(1)
-    
+
     for _ in range(1000):
         # generate random numbers between 0-1
         value = random()
@@ -34,8 +34,8 @@ def getExperimentalValues(cdfs):
         discreteVal = 0
 
         # Finding the smallest number which is larger than 'value'. It is actually upper bound.
-        # It can be solved using both Bruteforce and Binary Search. I used the latter. 
-        
+        # It can be solved using both Bruteforce and Binary Search. I used the latter.
+
         lo = 0
         hi = len(cdfs)-1
         while lo <= hi:
@@ -67,21 +67,34 @@ def getExperimentalValues(cdfs):
     return discreteValues, freqencies
 
 
+def getPx(x, Lambda):
+    if x < 0:
+        return 0
+    else:
+        return math.exp(-1.0 * Lambda) * math.pow(Lambda, x) / math.factorial(x)
+
+
 def main():
     ####### Theoratical values #######
     x = [i for i in range(0, 21)]
     Lambda = 1
-    cdfs = [getCDF(i, Lambda) for i in x]
 
+    ######### p(x) ###########
+
+    Px = [getPx(i, Lambda) for i in x]
+    plt.plot(x, Px, label="x vs P(x)")
+
+    ########## F(x) ###########
+    cdfs = [getCDF(i, Lambda) for i in x]
     plt.plot(x, cdfs, label="x vs F(x)")
 
     ######## Experimental values #########
     # print(cdfs)
 
     discreteValues, cummulative_freq = getExperimentalValues(cdfs)
-
     plt.plot(x, cummulative_freq, label="x vs cummulative frequencies")
 
+    ############# Plot them ###############
     plt.xlabel('x - axis')
     plt.ylabel('y - axis')
     plt.title('Poisson')
